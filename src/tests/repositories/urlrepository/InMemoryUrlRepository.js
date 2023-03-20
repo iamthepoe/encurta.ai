c1510c2c05b157c63501f429660328abb2c070ae
iamthepoe <lucagamerbrime@gmail.com>
2023-03-20 19:09:58 -0300

    creating in memory urlRepository for future tests

---
+++ src/tests/repositories/urlrepository/InMemoryUrlRepository.js
@@ -0,0 +1,48 @@
+class InMemoryUrlRepository {
+	items = [];
+
+	async create(data) {
+		const { originalUrl, userId, hash } = data;
+		try {
+			const url = { id: items.length, originalUrl, userId, hash };
+			this.items.push(url);
+			return url;
+		} catch (e) {
+			throw new Error(e);
+		}
+	}
+
+	async update(id, data) {
+		try {
+			const index = this.items.findIndex((item) => item.id === id);
+			if (index !== -1) this.items[index] = data;
+			if (index === -1) throw new Error(`this url doesn't exist!`);
+			return true;
+		} catch (e) {
+			throw new Error(e);
+		}
+	}
+
+	find(data) {
+		try {
+			const item = this.items.find((item) => item === data);
+			if (!item) throw new Error(`this url doesn't exist!`);
+			return item;
+		} catch (e) {
+			throw new Error(e);
+		}
+	}
+
+	delete(id) {
+		try {
+			const index = this.items.findIndex((item) => item.id === id);
+			if (index !== -1) this.items.splice(index, 1);
+			if (index === -1) throw new Error(`this url doesn't exist!`);
+			return true;
+		} catch (e) {
+			throw new Error(e);
+		}
+	}
+}
+
+module.exports = InMemoryUrlRepository;
