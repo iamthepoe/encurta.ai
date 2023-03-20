b4584039c7afc3fa73e4b4e2f0ca9b3617e6ec4e
iamthepoe <lucagamerbrime@gmail.com>
2023-03-20 18:21:21 -0300

    changing collection properties names

--- src/models/UrlModel.js
+++ src/models/UrlModel.js
@@ -1,8 +1,8 @@
 const mongoose = require('../database/connection.js');
 
 const UrlSchema = new mongoose.Schema({
-	original_url: { type: String, required: true },
-	user_id: { type: String, required: true },
+	originalUrl: { type: String, required: true },
+	userId: { type: String, required: true },
 	hash: { type: String, required: true },
 });
 
