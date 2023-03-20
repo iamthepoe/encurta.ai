12beca820a257fcf6a17254223baafcc61af8749
iamthepoe <lucagamerbrime@gmail.com>
2023-03-20 18:21:46 -0300

    adding create method on urlrepository

--- src/repositories/UrlRepository.js
+++ src/repositories/UrlRepository.js
@@ -2,6 +2,20 @@ class UrlRepository {
 	constructor(model) {
 		this.model = model;
 	}
+
+	async create(data) {
+		const { originalUrl, userId, hash } = data;
+		try {
+			let queryData = await this.model.create({
+				originalUrl,
+				userId,
+				hash,
+			});
+			return queryData;
+		} catch (e) {
+			throw new Error(e);
+		}
+	}
 }
 
 module.exports = UrlRepository;
