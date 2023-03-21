2dd381340c29b0cdac07eb70be65a769a7b54a1c
iamthepoe <lucagamerbrime@gmail.com>
2023-03-20 21:34:07 -0300

    create hash model

---
+++ src/models/HashModel.js
@@ -0,0 +1,10 @@
+const mongoose = require('../database/connection.js');
+
+const HashSchema = new mongoose.Schema({
+	hash: { type: String, required: true },
+	inUse: { type: Boolean, default: false },
+});
+
+HashSchema.set('timestamps', true);
+
+module.exports = mongoose.model('hash', HashSchema);
