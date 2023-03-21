d21f4a62c21563aeaf8bdc18507e091c365e6861
iamthepoe <lucagamerbrime@gmail.com>
2023-03-20 21:57:13 -0300

    create hashservice

---
+++ src/services/HashService.js
@@ -0,0 +1,16 @@
+const response = require('../factories/ResponseFactory.js');
+
+class HashService {
+	constructor(repository) {
+		this.repository = repository;
+	}
+
+	async findFreeHash() {
+		try {
+			const hash = await this.repository.findOne({ inUse: false });
+			return response(200, 'Finded hash', false, { hash });
+		} catch (e) {
+			return response(400, e, true);
+		}
+	}
+}
