6851602c6662c1ef862becc8fe2bc5138b312c2b
iamthepoe <lucagamerbrime@gmail.com>
2023-03-20 21:30:21 -0300

    initializing the create method of url service

--- src/services/UrlService.js
+++ src/services/UrlService.js
@@ -4,4 +4,10 @@ class UrlService {
 	constructor(repository) {
 		this.repository = repository;
 	}
+
+	/*async create(data) {
+		const { originalUrl, userId } = data;
+		if (!originalUrl.trim() || !userId.trim())
+			return response(400, true, 'You have empty fields!');
+	}*/
 }
