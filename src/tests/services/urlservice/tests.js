028fb7bdec42984fa51a3c4f60f056b0047b4db3
iamthepoe <lucagamerbrime@gmail.com>
2023-03-22 20:23:18 -0300

    changing 'test' to 'it' and adding new tests'

--- src/tests/services/urlservice/tests.js
+++ src/tests/services/urlservice/tests.js
@@ -1,4 +1,4 @@
-const { test, mock } = require('node:test');
+const { it } = require('node:test');
 const assert = require('node:assert');
 const response = require('../../../factories/ResponseFactory.js');
 const UrlService = require('../../../services/UrlService.js');
@@ -10,7 +10,7 @@ const inMemoryHashRepository = new InMemoryHashRepository();
 const hashService = new HashService(response, inMemoryHashRepository);
 const urlService = new UrlService(response, inMemoryUrlRepository, hashService);
 
-test('create a new short url', async () => {
+it('should create a new short url', async () => {
 	const response = await urlService.create({
 		originalUrl: 'www.google.com',
 		userId: 'fe6eebb7-36f4-487e-aae4-415c35e4d4e',
@@ -21,10 +21,29 @@ test('create a new short url', async () => {
 	assert.ok(body.data);
 });
 
-test('find original link by the short url', async () => {
+it('should prevent to create a new short url with empty values', async () => {
+	const response = await urlService.create({
+		originalUrl: '    ',
+		userId: '',
+	});
+	const { status, body, error } = response;
+	assert.strictEqual(status, 400);
+	assert.strictEqual(error, true);
+	assert.ok(!body.data);
+});
+
+it('should find original link by the short url', async () => {
 	const response = await urlService.findUrl('XzS/np4');
 	const { status, body, error } = response;
 	assert.strictEqual(status, 200);
 	assert.strictEqual(error, false);
 	assert.ok(body.data.originalUrl);
+});
+
+it('should prevent to find a original link with request empty values', async () => {
+	const response = await urlService.findUrl('   ');
+	const { status, body, error } = response;
+	assert.strictEqual(status, 400);
+	assert.strictEqual(error, true);
+	assert.ok(!body.data?.originalUrl);
 });
