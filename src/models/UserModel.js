151ee25f0c3c731ea8a60d0fa433a1b669c48253
iamthepoe <lucagamerbrime@gmail.com>
2023-03-26 11:01:05 -0300

    create user model

---
+++ src/models/UserModel.js
@@ -0,0 +1,11 @@
+const mongoose = require('../database/connection.js');
+
+const UserSchema = new mongoose.Schema({
+	name: { type: String, required: true },
+	email: { type: String, required: true, unique: true },
+	password: { type: String, required: true },
+});
+
+UserSchema.set('timestamps', true);
+
+module.exports = mongoose.model('urls', UserSchema);
