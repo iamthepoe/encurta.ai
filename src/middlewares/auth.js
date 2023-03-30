03c40c8dcd447119b0da4fb42240b49b435e87ef
iamthepoe <lucagamerbrime@gmail.com>
2023-03-30 19:11:11 -0300

    create authorization middleware

---
+++ src/middlewares/auth.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const secret = process.env.JWT_SECRET;
+
+async function authUser(req, res, next) {
+	const authToken = req.headers['authorization'];
+	if (authToken != undefined) {
+		const token = authToken.split(' ')[1];
+		try {
+			const decoded = await jwt.verify(token, secret);
+			req.decodedToken = decoded;
+			next();
+		} catch (e) {
+			return res
+				.status(403)
+				.json({ error: 'true', message: 'Invalid token!' });
+		}
+	} else {
+		return res.status(403).json({ error: true, message: 'Invalid token!' });
+	}
+}
+
+module.exports = authUser;
