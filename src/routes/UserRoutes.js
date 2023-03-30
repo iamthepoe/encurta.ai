5bf1b189700f2403d1ba617299bca365fe0ab611
iamthepoe <lucagamerbrime@gmail.com>
2023-03-30 14:26:40 -0300

    add register route

---
+++ src/routes/UserRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const userFactory = require('../factories/UserFactory.js');
+const userService = userFactory();
+
+router.post('/users', async (req, res) => {
+	const { name, email, password } = req.body;
+	const result = await userService.create({ name, email, password });
+	const { status, body, error, message } = result;
+	return res.status(status).json({ body, error, message });
+});
+
+module.exports = router;
